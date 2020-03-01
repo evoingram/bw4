4aa47f9dcb57d4042b8ad95c38d1c77a62ab3338
Erica L. Ingram <inquiries@aquoco.co>
2020-02-29 21:00:21 -0800

    fixed migration

--- data/migrations/20200228204611_users.js
+++ data/migrations/20200228204611_users.js
@@ -2,11 +2,6 @@ exports.up = function(knex) {
 	return knex.schema.createTable('users', users => {
 		users.increments('usersid');
 
-		users
-			.string('username', 128)
-			.notNullable()
-			.unique();
-
 		users.string('password', 128).notNullable();
 
 		users
