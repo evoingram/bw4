9877f81a1d4391e6e50f63f512d20fcd6c874e11
Erica L. Ingram <inquiries@aquoco.co>
2020-02-29 21:03:48 -0800

    fixed db connection

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 	development: {
 		client: 'pg',
 		connection:
-			'postgres://rnubomyahmrzsd:554b0f134597a793106f80452338fcc87092962f63beb3fdd5ef58d34792a7fb@ec2-18-213-176-229.compute-1.amazonaws.com:5432/d848goo2rrk72h',
+			'postgres://hojqyvewmiwxrk:8c4134cf7dea9444e1cca94fcb92b5ba072519ed05792efc290ddc91b580b615@ec2-54-80-184-43.compute-1.amazonaws.com:5432/ddrr8ser2jhafp',
 		migrations: {
 			directory: './data/migrations'
 		},
@@ -19,7 +19,7 @@ module.exports = {
 	production: {
 		client: 'pg', // postgresql
 		connection:
-			'postgres://rnubomyahmrzsd:554b0f134597a793106f80452338fcc87092962f63beb3fdd5ef58d34792a7fb@ec2-18-213-176-229.compute-1.amazonaws.com:5432/d848goo2rrk72h',
+			'postgres://hojqyvewmiwxrk:8c4134cf7dea9444e1cca94fcb92b5ba072519ed05792efc290ddc91b580b615@ec2-54-80-184-43.compute-1.amazonaws.com:5432/ddrr8ser2jhafp',
 		pool: {
 			min: 2,
 			max: 10
