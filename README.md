cc240587bc1324e095fc712402c6145551b8e5e1
Erica L. Ingram <inquiries@aquoco.co>
2020-03-01 17:55:03 -0800

    user/tickets tests tentatively done

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ How to get a two:
 How to get a three:
 -----------------------------------------------------------
 
-- [ ] Build an integrated slack-bot that allows students to submit help tickets through slack. Allow the ability to subscribe to the Queue in slack to be notified if someone opens a ticket.
+- [X] Build an integrated slack-bot that allows students to submit help tickets through slack. Allow the ability to subscribe to the Queue in slack to be notified if someone opens a ticket.
 	Slack action to create ticket -- > send from Slack API to our API
 	On ticket creation or ticket status change, send notification to slack api
 	Slack API notifies Slackbot that there's a new ticket/status change
@@ -45,7 +45,7 @@ How to get a three:
 
 - [X] Make it so a user can be both a student and a helper.
 
-- [ ] The project has end to end tests using a test database.
+- [X] The project has end to end tests using a test database.
 	https://learning.postman.com/docs/postman/api-documentation/documenting-your-api/
 
 - [X] Student added authorization and authentication services. 
--- users/users.spec.js
+++ users/users.spec.js
@@ -2,23 +2,26 @@ const request = require('supertest');
 
 const server = require('../api/server.js');
 
-describe('cohorts router', function() {
+describe('users router', function() {
 	it('should run the tests', function() {
 		expect(true).toBe(true);
 	});
 
-	describe('GET /api/cohorts', function() {
+	// get list of users
+	// router.get('/', restricted, (req, res) => {
+
+	describe('GET /api/users', function() {
 		it('should return 200 OK', function() {
 			return request(server)
-				.get('/api/cohorts')
+				.get('/api/users')
 				.then(res => {
 					expect(res.status).toBe(200);
 				});
 		});
 
-		it('should return cohorts as the router value', function() {
+		it('should return users as the router value', function() {
 			return request(server)
-				.get('/api/cohorts')
+				.get('/api/users')
 				.then(res => {
 					expect(Array.isArray(res.body)).toBe(true);
 				});
@@ -26,33 +29,69 @@ describe('cohorts router', function() {
 
 		it('should return JSON formatted body', function() {
 			return request(server)
-				.get('/api/cohorts')
+				.get('/api/users')
 				.then(res => {
 					expect(res.type).toMatch(/json/);
 				});
 		});
 
-		it('should return an array of cohorts (async version)', async function() {
-			const res = await request(server).get('/api/cohorts');
+		it('should return an array of users (async version)', async function() {
+			const res = await request(server).get('/api/users');
 
 			expect(Array.isArray(res.body)).toBe(true);
 		});
 	});
 
-	describe('POST /api/cohorts', function() {
-		// 	Cohorts.addCohort(resourceData);
-		let testCohort = { cohort: 'test123' };
+	// get individual user
+	// router.get('/:id', restricted, (req, res) => {
+
+	describe('GET /api/users/1', function() {
+		it('should return 200 OK', function() {
+			return request(server)
+				.get('/api/users/1')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+
+		it('should return users as the router value', function() {
+			return request(server)
+				.get('/api/users/1')
+				.then(res => {
+					expect(Array.isArray(res.body)).toBe(true);
+				});
+		});
+
+		it('should return JSON formatted body', function() {
+			return request(server)
+				.get('/api/users/1')
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+
+		it('should return an array of users (async version)', async function() {
+			const res = await request(server).get('/api/users/1');
+
+			expect(Array.isArray(res.body)).toBe(true);
+		});
+	});
+
+	// create user
+	describe('POST /api/users', function() {
+		// 	Users.addUser(resourceData);
+		let testUser = { user: 'test123' };
 		it('should return 201 OK', async function() {
 			return await request(server)
-				.post('/api/cohorts', testCohort)
+				.post('/api/users', testUser)
 				.then(res => {
 					expect(res.status).toBe(201);
 				});
 		});
 
-		it('should return cohorts as the router value', async function() {
+		it('should return users as the router value', async function() {
 			return await request(server)
-				.post('/api/cohorts', testCohort)
+				.post('/api/users', testUser)
 				.then(res => {
 					expect(Array.isArray(res.body)).toBe(true);
 				});
@@ -60,32 +99,33 @@ describe('cohorts router', function() {
 
 		it('should return JSON formatted body', function() {
 			return request(server)
-				.post('/api/cohorts', testCohort)
+				.post('/api/users', testUser)
 				.then(res => {
 					expect(res.type).toMatch(/json/);
 				});
 		});
 
-		it('should return an array of cohorts (async version)', async function() {
-			const res = await request(server).post('/api/cohorts', testCohort);
+		it('should return an array of users (async version)', async function() {
+			const res = await request(server).post('/api/users', testUser);
 
 			expect(Array.isArray(res.body)).toBe(true);
 		});
 	});
 
-	describe('PUT /api/cohorts', function() {
-		let testCohort = { cohort: 'test123456' };
+	// update user
+	describe('PUT /api/users/3', function() {
+		let testUser = { user: 'test123456' };
 		it('should return 200 OK', async function() {
 			return await request(server)
-				.put('/api/cohorts', testCohort)
+				.put('/api/users/3', testUser)
 				.then(res => {
 					expect(res.status).toBe(200);
 				});
 		});
 
-		it('should return cohorts as the router value', async function() {
+		it('should return users as the router value', async function() {
 			return await request(server)
-				.put('/api/cohorts', testCohort)
+				.put('/api/users/3', testUser)
 				.then(res => {
 					expect(Array.isArray(res.body)).toBe(true);
 				});
@@ -93,32 +133,33 @@ describe('cohorts router', function() {
 
 		it('should return JSON formatted body', async function() {
 			return await request(server)
-				.put('/api/cohorts', testCohort)
+				.put('/api/users/3', testUser)
 				.then(res => {
 					expect(res.type).toMatch(/json/);
 				});
 		});
 
-		it('should return an array of cohorts (async version)', async function() {
-			const res = await request(server).put('/api/cohorts', testCohort);
+		it('should return an array of users (async version)', async function() {
+			const res = await request(server).put('/api/users/3', testUser);
 
 			expect(Array.isArray(res.body)).toBe(true);
 		});
 	});
 
-	describe('DELETE /api/cohorts', function() {
-		let testCohort = { cohortsid: 6 };
+	// delete user
+	describe('DELETE /api/users', function() {
+		let testUser = { usersid: 6 };
 		it('should return 201 OK', async function() {
 			return await request(server)
-				.delete('/api/cohorts', testCohort)
+				.delete('/api/users', testUser)
 				.then(res => {
 					expect(res.status).toBe(201);
 				});
 		});
 
-		it('should return cohorts as the router value', async function() {
+		it('should return users as the router value', async function() {
 			return await request(server)
-				.delete('/api/cohorts', { cohortsid: 5 })
+				.delete('/api/users', { usersid: 5 })
 				.then(res => {
 					expect(Array.isArray(res.body)).toBe(true);
 				});
@@ -126,34 +167,199 @@ describe('cohorts router', function() {
 
 		it('should return JSON formatted body', async function() {
 			return await request(server)
-				.delete('/api/cohorts', { cohortsid: 4 })
+				.delete('/api/users', { usersid: 4 })
 				.then(res => {
 					expect(res.type).toMatch(/json/);
 				});
 		});
 
-		it('should return an array of cohorts (async version)', async function() {
-			const res = await request(server).delete('/api/cohorts', { cohortsid: 3 });
+		it('should return an array of users (async version)', async function() {
+			const res = await request(server).delete('/api/users', { usersid: 3 });
 
 			expect(Array.isArray(res.body)).toBe(true);
 		});
 	});
 });
 
-// get all roles -- router.get('/', restricted, (req, res) => {
-// get individual role -- router.get('/:rolesid', restricted, (req, res) => {
-// create role -- router.post('/', restricted, (req, res) => {
-// update role -- router.put('/:rolesid', restricted, (req, res) => {
-// delete role -- router.delete('/:id', restricted, (req, res) => {
+describe('roles router', function() {
+	it('should run the tests', function() {
+		expect(true).toBe(true);
+	});
 
-/*
--- delete user
-DELETE FROM Users where Users.usersid = '';
-*/
-// router.delete('/:id', restricted, (req, res) => {
+	// get list of roles
+	// router.get('/', restricted, (req, res) => {
+	describe('GET /api/roles', function() {
+		it('should return 200 OK', function() {
+			return request(server)
+				.get('/api/roles')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
 
-// get individual user
-// router.get('/:id', restricted, (req, res) => {
+		it('should return roles as the router value', function() {
+			return request(server)
+				.get('/api/roles')
+				.then(res => {
+					expect(Array.isArray(res.body)).toBe(true);
+				});
+		});
 
-// get list of users
-// router.get('/', restricted, (req, res) => {
+		it('should return JSON formatted body', function() {
+			return request(server)
+				.get('/api/roles')
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+
+		it('should return an array of roles (async version)', async function() {
+			const res = await request(server).get('/api/roles');
+
+			expect(Array.isArray(res.body)).toBe(true);
+		});
+	});
+
+	// get individual role -- router.get('/:rolesid', restricted, (req, res) => {
+	describe('GET /api/roles/1', function() {
+		it('should return 200 OK', function() {
+			return request(server)
+				.get('/api/roles/1')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+
+		it('should return roles/1 as the router value', function() {
+			return request(server)
+				.get('/api/roles/1')
+				.then(res => {
+					expect(Array.isArray(res.body)).toBe(true);
+				});
+		});
+
+		it('should return JSON formatted body', function() {
+			return request(server)
+				.get('/api/roles/1')
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+
+		it('should return an array of roles (async version)', async function() {
+			const res = await request(server).get('/api/roles/1');
+
+			expect(Array.isArray(res.body)).toBe(true);
+		});
+	});
+
+	// create role -- router.post('/', restricted, (req, res) => {
+	describe('POST /api/roles', function() {
+		// 	Roles.addRole(resourceData);
+		let testRole = { role: 'test123' };
+		it('should return 201 OK', async function() {
+			return await request(server)
+				.post('/api/roles', testRole)
+				.then(res => {
+					expect(res.status).toBe(201);
+				});
+		});
+
+		it('should return roles as the router value', async function() {
+			return await request(server)
+				.post('/api/roles', testRole)
+				.then(res => {
+					expect(Array.isArray(res.body)).toBe(true);
+				});
+		});
+
+		it('should return JSON formatted body', function() {
+			return request(server)
+				.post('/api/roles', testRole)
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+
+		it('should return an array of roles (async version)', async function() {
+			const res = await request(server).post('/api/roles', testRole);
+
+			expect(Array.isArray(res.body)).toBe(true);
+		});
+	});
+
+	// update role -- router.put('/:rolesid', restricted, (req, res) => {
+	describe('PUT /api/roles/3', function() {
+		let testRole = { role: 'test123456' };
+		it('should return 200 OK', async function() {
+			return await request(server)
+				.put('/api/roles/3', testRole)
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+
+		it('should return roles as the router value', async function() {
+			return await request(server)
+				.put('/api/roles/3', testRole)
+				.then(res => {
+					expect(Array.isArray(res.body)).toBe(true);
+				});
+		});
+
+		it('should return JSON formatted body', async function() {
+			return await request(server)
+				.put('/api/roles/3', testRole)
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+
+		it('should return an array of roles (async version)', async function() {
+			const res = await request(server).put('/api/roles/3', testRole);
+
+			expect(Array.isArray(res.body)).toBe(true);
+		});
+	});
+
+	// delete role -- router.delete('/:id', restricted, (req, res) => {
+
+	/*
+		-- delete role
+			DELETE FROM Roles where Roles.rolesid = '';
+	*/
+	// router.delete('/:id', restricted, (req, res) => {
+
+	describe('DELETE /api/roles', function() {
+		let testRole = { rolesid: 3 };
+		it('should return 201 OK', async function() {
+			return await request(server)
+				.delete('/api/roles', testRole)
+				.then(res => {
+					expect(res.status).toBe(201);
+				});
+		});
+
+		it('should return roles as the router value', async function() {
+			return await request(server)
+				.delete('/api/roles', { rolesid: 3 })
+				.then(res => {
+					expect(Array.isArray(res.body)).toBe(true);
+				});
+		});
+
+		it('should return JSON formatted body', async function() {
+			return await request(server)
+				.delete('/api/roles', { rolesid: 3 })
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+
+		it('should return an array of roles (async version)', async function() {
+			const res = await request(server).delete('/api/roles', { rolesid: 3 });
+
+			expect(Array.isArray(res.body)).toBe(true);
+		});
+	});
+});
